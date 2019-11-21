5ef45c97d579aadf0026bf02c29d135934b2764b
DominicWuest <dominic.wuest@hotmail.com>
2019-11-21 16:24:23 +0100

    Added syncing of time

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+// ---------- Begin of Imports ----------
+
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var path = require('path');
+var time = require('time');
 
+// ----------- End of Imports -----------
+
+// Listen on the port 3000
 server.listen(3000);
 
 // Directory for static files
@@ -13,10 +19,12 @@ app.set('views', path.join(__dirname, 'public/views'));
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
 
+// Routing for the homepage
 app.get('/', function(req, res) {
   res.render('index.ejs', {});
 });
 
+// Routing for the page where clients can watch videos together
 app.get('/watch', function(req, res) {
   res.render('watch.ejs', {})
 });
@@ -24,9 +32,36 @@ app.get('/watch', function(req, res) {
 // Namespace for /watch
 var watch = io.of('/watch')
 
+// An integer indicating the time of the video which has passed when the last timeChange was recorded
+let lastDuration = 0;
+// An integer indicating the time when the last timeChange was recorded
+let lastDurationTime = 0;
+// An integer indicating the amount of clients which are connected
+let connectedClients = 0;
+
+// Gets called when a new clients connects to the socket
 watch.on('connection', function(socket) {
-    socket.
-    on('stateChange', function(data) {
-      socket.broadcast.emit('stateChange', data);
-    });
-  });
+  // Increment the amount of connected clients when a new client connects
+  connectedClients++;
+  socket
+  // Gets called whenever the state of one of the clients players changes
+  .on('stateChange', function(data) {
+    socket.broadcast.emit('stateChange', data);
+  })
+  // Gets called whenever a client changes the time of their video
+  .on('timeChange', function(data) {
+    socket.broadcast.emit('timeChange', data);
+    // Set lastDuration to the current time sent by the user
+    lastDuration = data;
+    // Set the last time whewn the duration was updated to the current time
+    lastDurationTime = time.time();
+  })
+  // Gets called by clients when they want to sync the time of the video of their player to the ones of the other clients
+  .on('requestTimeSync', function() {
+    if (connectedClients === 1) socket.emit('timeChange', 0);
+    // Sends the current time of the player to the client which requested it
+    else socket.emit('timeChange', lastDuration + time.time() - lastDurationTime);
+  })
+  // Decrement the amount of connected clients when one disconnects
+  .on('disconnect', function() { connectedClients--; });
+});
--- public/js/watch.js
+++ public/js/watch.js
@@ -1,20 +1,28 @@
+// Connect to the socket
 let socket = io.connect('http://localhost:3000/watch');
 
+// A boolean indicating whether the change of the state got caused by the user or an external client
 let externalChange = false;
 
+// An integer indicating the last state the player had
 let lastState = -1;
 
 // Socket message listeners
 
+socket
 // Gets called whenever the state of the player of any client changes
-socket.on('stateChange', function(data) {
-  if (data[0] === player.getPlayerState()) return;
+.on('stateChange', function(data) {
   externalChange = true;
-  switch (data[0]) {
+  // Resume the video if the new state is 1 or pause the video if the new state is 2
+  switch (data) {
     case 1: player.playVideo(); break;
     case 2: player.pauseVideo(); break;
   }
-  player.seekTo(data[1], true);
+})
+// Gets called whenever another user changes the time of the video or the user requests to sync the time
+.on('timeChange', function(data) {
+  player.seekTo(data, true);
+  if (data !== 0) player.playVideo();
 });
 
 // Event listeners
@@ -24,14 +32,26 @@ function startVideo() {
   // Start and stop the video
   player.playVideo();
   player.pauseVideo();
+  // Sync the newly connected clients players time with the one of the other clients
+  requestTimeSync();
   // Add the event listener for stateChange
   player.addEventListener('onStateChange', 'synchPlayerStates');
 }
 
 // Gets called whenever the player state changes
 function synchPlayerStates(data) {
-  // TODO: Sync after buffering
-  // Only emit stateChange event if the change doesn't come from an emitted event itself and the user stopped or resumed the video
-  if (!externalChange && (data.data === 1 || data.data === 2)) socket.emit('stateChange', [data.data, player.getCurrentTime()]);
-  else externalChange = false;
+  let state = data.data;
+  // Only emit events if the change doesn't come from an emitted event itself
+  if (externalChange) externalChange = false;
+  // Synch the states of the clients players if the user stopped or resumed the video
+  else if (state === 1 || state === 2) socket.emit('stateChange', data.data);
+  // Synch the time of the clients players if the a users video starts buffering
+  else if (state === 3 && lastState !== -1) socket.emit('timeChange', player.getCurrentTime());
+  // Set the last state to the current state
+  lastState = state;
+}
+
+// Gets called whenever the page finishes loading in order to sync the time with the one from the other users
+function requestTimeSync() {
+  socket.emit('requestTimeSync');
 }
