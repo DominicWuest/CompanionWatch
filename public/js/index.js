213a4bb9f8566bb0ede3b298cb041b2fc2a92969
DominicWuest <dominic.wuest@hotmail.com>
2019-12-16 16:21:14 +0100

    Added message for no existing rooms

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,11 @@
+// Gets called when the body has finished loading
+function checkRooms() {
+  // Check if there are any rooms available
+  let rooms = document.getElementsByClassName('room');
+  // If there are zero rooms
+  if (!rooms.length) document.getElementById('roomWrapper').innerHTML = '<p class="noRoomsMessage">Currently there are no existing rooms</p>';
+}
+
 // A function which creates a new room and redirects the user to it
 function createRoom() {
   // Send a post request to create a new room
