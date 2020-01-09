f6ecec6f7e681f34a200747203a64f181d9ebec0
DominicWuest <dominic.wuest@hotmail.com>
2020-01-09 23:41:19 +0100

    Revert "Fixed video not playing when clicking on new one and fixed synching of videos"
    This reverts commit 194f46a60f8c017e6c09522b836dfba247233856.

--- public/js/roomWatch.js
+++ public/js/roomWatch.js
@@ -1,4 +1,4 @@
- // Connect to the socket
+// Connect to the socket
 let socket = io.connect(window.location.origin + '/watch', {query : 'ns=' + window.location.href.split('/').slice(-1)[0]});
 
 // A boolean indicating whether the change of the state got caused by the user or an external client
@@ -96,11 +96,9 @@ socket
 })
 // Gets called whenever another user requests a new video
 .on('videoChange', function(id) {
-  // Load the new video
+  // Pause the player and load the new video
   player.loadVideoById(id);
-  externalChange = true;
   player.pauseVideo();
-  player.playVideo();
 });
 
 // Event listeners
@@ -140,6 +138,8 @@ function loadVideoById(videoId) {
   // If the id doesn't match the current id
   if (videoId !== currentId) {
     player.loadVideoById(videoId);
+    externalChange = true;
+    player.pauseVideo();
     socket.emit('videoChange', videoId);
   }
 }
